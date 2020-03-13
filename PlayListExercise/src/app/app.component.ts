17786dea4edf46ace8c90f0301d92a4b5384ed29
Arthur MICHEL <amichel@talent-bs.com>
2020-03-13 02:41:32 +0100

    Ended PlayListManagement Test

--- PlayListExercise/src/app/app.component.ts
+++ PlayListExercise/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,111 @@ export class AppComponent {
 
   playlists: Playlist[] = new PlaylistComponent().playlists;
 
+  editing:boolean = false;
+  adding:boolean = false;
+
+  newPlaylist : Playlist ={
+    name: "",
+    totalDuration: 0,
+    totalSongs: 0,
+    description: "",
+    songs : [],
+  }
+  newSong:Song= {
+    title :"",
+    duration:0,
+    artist:""
+  };
+
+
+  countSong(playlist){
+    return playlist.songs.length;
+  }
+  countDuration(playlist){
+    var c = 0;
+    playlist.songs.forEach((song : Song) => {
+      c+=song.duration;
+    });
+    return c;
+  }
+
+  startAdding(){
+    this.adding = true;
+  }
+
+  cancel(){
+    this.editing=false;
+    this.adding=false;
+  }
+
+  add(){
+    this.newPlaylist.songs.push(this.newSong)
+    this.newSong = {
+      title :"",
+      duration:0,
+      artist:""
+    };
+  }
+
+
+  deletePlaylist(i){
+    this.playlists.splice(i,1)
+  }
+
+  deleteSong(i){
+    this.newPlaylist.songs.splice(i,1)
+  }
+
+  editPlaylist(i){
+    this.editing=true;
+    this.newPlaylist = {...this.playlists[i]}
+
+    
+  }
+
+  submit(){
+    if(this.adding){
+      this.playlists.push(this.newPlaylist);
+    }if(this.editing){
+      this.playlists.forEach(playlist => {
+        if(playlist.name = this.newPlaylist.name){
+          playlist = this.newPlaylist
+        }
+      });
+    }
+    this.newPlaylist={
+      name: "",
+      totalDuration: 0,
+      totalSongs: 0,
+      description: "",
+      songs : [],
+    }
+    this.editing = false;
+  }
+
+  
+  submitDisabled(){
+    for (let i = 0; i < this.newPlaylist.songs.length; i++) {
+      if(!(this.newPlaylist.songs[i].duration>0)){
+      
+        return true
+      }
+      if(this.newPlaylist.songs[i].artist.trim()=="")
+        return true
+        if(this.newPlaylist.songs[i].title.trim()=="")
+        return true
+    }
+      if(this.newPlaylist.name.trim()==""){
+     
+        return true;
+      }
+
+      if(this.newPlaylist.description.trim()==""){
+      
+        return true;
+      }
+      return false
+  }
 
 
 }
